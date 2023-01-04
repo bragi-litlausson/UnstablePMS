d551f9103ae82af4733c7d439f043f62305d110e
Bragi Litlausson <bragi@unstabledemo.space>
2023-01-04 20:24:49 +0100

    Add description of project command

--- cmd/project/project.go
+++ cmd/project/project.go
@@ -7,13 +7,9 @@ import (
 
 var ProjectCmd = &cobra.Command{
 	Use:   "project",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Manage project",
+	Long: `Allows to set up or edit project 
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		wizard.StartWizard()
 	},
